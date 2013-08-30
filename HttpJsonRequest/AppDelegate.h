2fdf59aa1b25df75115939c04e5bb2323d3ab97f
Kevin Thornton <kevint@clear.net>
2013-08-29 17:58:45 -0700

    Initial Commit

---
+++ HttpJsonRequest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HttpJsonRequest
+//
+//  Created by Kevin Thornton on 8/29/13.
+//  Copyright (c) 2013 Kevin Thornton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HttpJsonRequest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HttpJsonRequest
+//
+//  Created by Kevin Thornton on 8/29/13.
+//  Copyright (c) 2013 Kevin Thornton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
